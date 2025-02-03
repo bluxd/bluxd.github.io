abb48ef01f7426b348a11d9f172cdf8c2c2a9c29
David Garcia <david.ed.g2@gmail.com>
2025-02-03 11:13:09 -0600

    changes

--- ia1/tarea1/cleaner.js
+++ ia1/tarea1/cleaner.js
@@ -1,5 +1,3 @@
-// MIT License
-
 function reflex_agent(location, state){
     if (state === "DIRTY") return "CLEAN";
     else if (location === "A") return "RIGHT";
@@ -10,16 +8,36 @@ function test(states){
     const location = states[0];
     const state = states[0] === "A" ? states[1] : states[2];
     const action_result = reflex_agent(location, state);
-    document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result).concat(" - ").concat(states);
+
+    document.getElementById("log").innerHTML+="<br>".concat(states).concat(" --- ").concat("Location: ").concat(location).concat(" | Action: ").concat(action_result);
+
+    visitedStates.add(states.join(","));
+    if (visitedStates.size === 8) {
+        document.getElementById("log").innerHTML += "<br><b>All states reached. Stopping simulation.</b>";
+        return;
+    }
+
     if (action_result === "CLEAN"){
         if (location === "A") states[1] = "CLEAN";
         else if (location === "B") states[2] = "CLEAN";
+    } else if (action_result === "RIGHT") {
+        states[0] = "B";
+    } else if (action_result === "LEFT") {
+        states[0] = "A";
     }
-    else if (action_result === "RIGHT") states[0] = "B";
-    else if (action_result === "LEFT") states[0] = "A";
-	
-    setTimeout(function(){ test(states); }, 2000);
+
+    states[1] = getRandomDirt(states[1]);
+    states[2] = getRandomDirt(states[2]);
+
+    setTimeout(function(){ test(states); }, 500);
 }
 
+function getRandomDirt(previousState) {
+    return Math.random() < (previousState === "CLEAN" ? 0.3 : 0.6) ? "DIRTY" : "CLEAN";
+}
+
+const visitedStates = new Set();
+
 const states = ["A", "DIRTY", "DIRTY"];
+
 test(states);
